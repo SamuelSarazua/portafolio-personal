7fdc5b85f286fc78dc458d0c79dc68fdb7249c00
Samue Sarazua <sesarazua@scl.edu.gt>
2024-09-26 12:03:31 -0600

    Se completa la primer seccion de formularios - Se agrega la GitHub Page - Se agrega el link de GitHub

--- componentes/projects/projects.js
+++ componentes/projects/projects.js
@@ -1,21 +1,47 @@
 import { header } from "../header/header.js";
+import { dataProyectos } from "../../data/data.js";
 
 function proyectos() {
 
     let section = document.createElement('section');
     section.className = 'section-proyectos'
     section.appendChild(header());  
-    section.appendChild(Lista());
+    section.appendChild(listaProyectos());
 
 
     return section;
 }
 
+function listaProyectos() {
+    let div = document.createElement('div');
+    div.className = "Lista-proyectos";
 
-function Lista() {
+    dataProyectos.forEach((proyecto)=>{
+        div.appendChild(item(proyecto.nombre,proyecto.github,proyecto.githubPage,proyecto.stacks));
+    });
+
+    return div;
+}
+
+function item(texto,github,githubPage,stacks) {
     let lista = document.createElement('div');
-    lista.className = 'lista-proyectos';
-    lista.innerText = "Item de Lista";
+    lista.className = "div-item"
+
+    let a = document.createElement('a');
+    a.className = "link-github-page"
+    a.href = githubPage;
+    a.innerText = "Formulario";
+    lista.appendChild(a);
+
+    let stack = document.createElement('div');
+    stack.innerText = "[js, css, HTML]";
+    lista.appendChild(stack);
+
+    let btnGithub = document.createElement('a');
+    btnGithub.className = "btn-github";
+    btnGithub.href = github;
+    btnGithub.innerHTML = "Github";
+    lista.appendChild(btnGithub);
 
     return lista;
 }
---
+++ data/data.js
@@ -0,0 +1,11 @@
+let dataProyectos = [
+    {
+        nombre: "Formularios",
+        descripcion: "text text text",
+        githubPage: "https://samuelsarazua.github.io/Proyecto--Formularios/",
+        github: "https://github.com/SamuelSarazua/Proyecto--Formularios",
+        stacks: ["js","css","html"]
+    },
+];
+
+export { dataProyectos }
