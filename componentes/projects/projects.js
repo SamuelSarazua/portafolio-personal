e3205fb29ccbba0a9bf51c065f547d654e76e9a1
Samue Sarazua <sesarazua@scl.edu.gt>
2024-10-10 08:07:33 -0600

    Se agregan los proyectos principales

--- componentes/projects/projects.js
+++ componentes/projects/projects.js
@@ -23,19 +23,32 @@ function listaProyectos() {
     return div;
 }
 
-function item(texto,github,githubPage,stacks) {
+function item(texto, github, githubPage, stacks) {
     let lista = document.createElement('div');
-    lista.className = "div-item"
+    lista.className = "div-item";
 
     let a = document.createElement('a');
-    a.className = "link-github-page"
+    a.className = "link-github-page";
     a.href = githubPage;
-    a.innerText = "Formulario";
+    a.innerText = texto;  
     lista.appendChild(a);
 
     let stack = document.createElement('div');
-    stack.innerText = "[js, css, HTML]";
+    stack.innerText = stacks.join(', ');  
     lista.appendChild(stack);
+
+    let descripcion = document.createElement('div');
+    if (texto === "Formularios") {
+        descripcion.innerText = "[sesarazua, bemorales]"; 
+    } else {
+        descripcion.innerText = "[sesarazua]"; 
+    }
+    lista.appendChild(descripcion);
+
+
+    let fecha = document.createElement('div');
+    fecha.innerText = "19/09/2024";
+    lista.appendChild(fecha);
 
     let btnGithub = document.createElement('a');
     btnGithub.className = "btn-github";
@@ -43,31 +56,11 @@ function item(texto,github,githubPage,stacks) {
     btnGithub.innerHTML = "Github";
     lista.appendChild(btnGithub);
 
-    return lista;
-}
-
-function item2(texto,github,githubPage,stacks) {
-    let lista = document.createElement('div');
-    lista.className = "div-item"
-
-    let a = document.createElement('a');
-    a.className = "link-github-page"
-    a.href = githubPage;
-    a.innerText = "Formulario";
-    lista.appendChild(a);
-
-    let stack = document.createElement('div');
-    stack.innerText = "[js, css, HTML]";
-    lista.appendChild(stack);
-
-    let btnGithub = document.createElement('a');
-    btnGithub.className = "btn-github";
-    btnGithub.href = github;
-    btnGithub.innerHTML = "Github";
-    lista.appendChild(btnGithub);
 
     return lista;
 }
+
+
 
 
 export { proyectos }
